906ebbabb912c71531a19b2088912f33e27e101f
Syed Ali <shahaddy1@gmail.com>
2026-05-03 22:28:18 +0500

    Prepare v0.24.9 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.kaizen.khushu"
         minSdk = 30
         targetSdk = 36
-        versionCode = 82
-        versionName = "0.24.8"
+        versionCode = 83
+        versionName = "0.24.9"
 
 
         buildConfigField("String", "AUDIO_BASE_URL", "\"https://example.com/audio/\"")
