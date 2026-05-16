09ea6744ff14ae828c642dae5632b9c3e0ccdddc
Syed Ali <shahaddy1@gmail.com>
2026-05-16 14:51:19 +0500

    chore(main): remove scratch polygon probes

--- app/src/main/java/com/kaizen/khushu/ui/util/scratch.kt
+++
@@ -1,6 +0,0 @@
-import androidx.graphics.shapes.RoundedPolygon
-import androidx.graphics.shapes.CornerRounding
-
-fun main() {
-    println("Test")
-}
--- app/src/main/java/com/kaizen/khushu/ui/util/test_poly.kt
+++
@@ -1,6 +0,0 @@
-import androidx.graphics.shapes.RoundedPolygon
-import androidx.graphics.shapes.CornerRounding
-fun main() {
-    val methods = RoundedPolygon.Companion::class.java.methods.map { it.name }
-    println(methods.joinToString(", "))
-}
